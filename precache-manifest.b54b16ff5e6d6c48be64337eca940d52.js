2322d01e32ad7061e638c5f2437a18e75546a519
andre-fajar-n <andrenugroho395@gmai.com>
2020-10-02 20:30:35 +0700

    Updates

---
+++ precache-manifest.b54b16ff5e6d6c48be64337eca940d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d01f172773f126e3830c327eb716a98",
+    "url": "/Profile-Page/index.html"
+  },
+  {
+    "revision": "57a71547a708003bea30",
+    "url": "/Profile-Page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d3e0b6212d5aaa56985",
+    "url": "/Profile-Page/static/js/2.7b48827d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Profile-Page/static/js/2.7b48827d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a71547a708003bea30",
+    "url": "/Profile-Page/static/js/main.e062a914.chunk.js"
+  },
+  {
+    "revision": "8fbffc053cc623a0bd9a",
+    "url": "/Profile-Page/static/js/runtime-main.0999cfe3.js"
+  }
+]);
