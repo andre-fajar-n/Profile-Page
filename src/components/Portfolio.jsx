731f4ed30d0ae25817b338cfcd5cd21664aad75b
andre-fajar-n <andrenugroho395@gmai.com>
2020-10-15 10:59:43 +0700

    styling portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "../style/css/Portfolio.css"
 
 const Portfolio = (props) => {
   var demo = props.data.demo
-  if (demo === "") {
-    demo = props.data.github
-  }
+
   return (
-    <div className={`single-portfolio col-sm-4 all ${props.data.category}`}>
-      <div className="relative">
-        <div className="thumb">
-          <div className="overlay overlay-bg"></div>
-          <img className="image img-fluid" src={props.data.image} alt="" />
-        </div>
-        <a href={demo} target="blank" className="img-pop-up">
-          <div className="middle">
-            <div className="text align-self-center d-flex"><img src={props.data.image} alt="" /></div>
+    <Fragment>
+      <div className={`single-portfolio col-sm-4 all ${props.data.category}`}>
+        <div className="relative">
+          <div className="thumb">
+            <a href={props.data.github} target="blank" className="img-pop-up">
+              <div className="overlay overlay-bg"></div>
+              <img className="image img-fluid no-hover" src={props.data.image} alt="" />
+              <img className="image img-fluid hover" src={props.data.image.replace("-dark", "")} alt="" />
+              {/* <div className="middle">
+                <div className="text align-self-center d-flex"><img src={props.data.image} alt="" /></div>
+              </div> */}
+            </a>
+            {demo === "" ? (
+              <Fragment></Fragment>
+            ) : (
+                <a href={demo} target="blank">
+                  <div className="cat demo">
+                    <div className="m-auto">View Demo</div>
+                  </div>
+                </a>
+              )}
           </div>
-        </a>
+        </div>
+        <div className="p-inner">
+          <h4>{props.data.title}</h4>
+          <div className="cat">{props.data.category}</div>
+        </div>
       </div>
-      <div className="p-inner">
-        <h4>{props.data.title}</h4>
-        <div className="cat">{props.data.category}</div>
-      </div>
-    </div>
+    </Fragment>
   )
 }
 
