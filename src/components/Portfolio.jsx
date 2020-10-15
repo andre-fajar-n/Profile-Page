3a7294b4319eed960d577ba4c16effb68b917221
andre-fajar-n <andrenugroho395@gmai.com>
2020-10-15 15:25:07 +0700

    edit style portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,22 +9,41 @@ const Portfolio = (props) => {
       <div className={`single-portfolio col-sm-4 all ${props.data.category}`}>
         <div className="relative">
           <div className="thumb">
-            <a href={props.data.github} target="blank" className="img-pop-up">
-              <div className="overlay overlay-bg"></div>
+            <a href={props.data.github} target="blank" className="img-pop-up inner">
+              {/* <div className="overlay overlay-bg"></div>
               <img className="image img-fluid no-hover" src={props.data.image} alt="" />
-              <img className="image img-fluid hover" src={props.data.image.replace("-dark", "")} alt="" />
-              {/* <div className="middle">
-                <div className="text align-self-center d-flex"><img src={props.data.image} alt="" /></div>
-              </div> */}
+              <img className="image img-fluid hover" src={props.data.image.replace("-dark", "")} alt="" /> */}
+              <div class="flip-box">
+                <div class="flip-box-inner">
+                  <div class="flip-box-front">
+                    <img className="image img-fluid no-hover" src={props.data.image} alt="" />
+                  </div>
+                  <div class="flip-box-back">
+                    <img className="image img-fluid hover" src={props.data.image.replace("-dark", "")} alt="" />
+                  </div>
+                </div>
+              </div>
             </a>
             {demo === "" ? (
               <Fragment></Fragment>
             ) : (
                 <a href={demo} target="blank">
-                  <div className="cat demo">
-                    <div className="m-auto">View Demo</div>
+                  <div class="demo">
+                    <div class="demo-inner">
+                      <div class="demo-front">
+                        <div>View Demo</div>
+                      </div>
+                      <div class="demo-back">
+                        <div>View Demo</div>
+                      </div>
+                    </div>
                   </div>
                 </a>
+                // <a href={demo} target="blank">
+                //   <div className="cat demo">
+                //     <div className="m-auto">View Demo</div>
+                //   </div>
+                // </a>
               )}
           </div>
         </div>
--- src/data/Portfolio.js
+++ src/data/Portfolio.js
@@ -3,70 +3,70 @@ import category from "./Category";
 var portfolio = [
     {
         "title": "Motor Speed Controller",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=motor-speed-controller",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=motor-speed-controller",
         "demo": "",
         "category": category[1],
         "github": "https://github.com/andre-fajar-n/motor-speed-controller"
     },
     {
         "title": "Chatbot Aturtagihan.com",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=Chatbot_WA_AturTagihan.com",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=Chatbot_WA_AturTagihan.com",
         "demo": "",
         "category": category[0],
         "github": "https://github.com/andre-fajar-n/Chatbot_WA_AturTagihan.com"
     },
     {
         "title": "Whatsapp Web Clone",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=Wa-web-clone",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=Wa-web-clone",
         "demo": "https://wa-web-clone.vercel.app/",
         "category": category[0],
         "github": "https://github.com/andre-fajar-n/Wa-web-clone"
     },
     {
         "title": "Embedded Electronics System",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=embedded-electronics-system",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=embedded-electronics-system",
         "demo": "",
         "category": category[1],
         "github": "https://github.com/andre-fajar-n/embedded-electronics-system"
     },
     {
         "title": "Andreino",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=FrontEnd-Ecommerce",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=FrontEnd-Ecommerce",
         "demo": "https://andreino.vercel.app/",
         "category": category[0],
         "github": "https://github.com/andre-fajar-n/FrontEnd-Ecommerce"
     },
     {
         "title": "Alta Soccer Game",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=Alta_Soccer_Game",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=Alta_Soccer_Game",
         "demo": "https://andre-fajar-n.github.io/Alta_Soccer_Game",
         "category": category[0],
         "github": "https://github.com/andre-fajar-n/Alta_Soccer_Game"
     },
     {
         "title": "Alterra Academy Clone",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=Clone_Alterra_Academy_Web",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=Clone_Alterra_Academy_Web",
         "demo": "https://andre-fajar-n.github.io/Clone_Alterra_Academy_Web",
         "category": category[0],
         "github": "https://github.com/andre-fajar-n/Clone_Alterra_Academy_Web"
     },
     {
         "title": "Hexapod Robot",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=Hexapod_Robot",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=Hexapod_Robot",
         "demo": "https://drive.google.com/file/d/1WwRouReJ9P6d5JnTYJYEWJwxoiFgsojx/view?usp=sharing",
         "category": category[1],
         "github": "https://github.com/andre-fajar-n/Hexapod_Robot"
     },
     {
         "title": "Computer Vision",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=Computer-Vision",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=Computer-Vision",
         "demo": "",
         "category": category[2],
         "github": "https://github.com/andre-fajar-n/Computer-Vision"
     },
     {
         "title": "Goal Keeper Robot",
-        "image": "https://github-readme-stats.vercel.app/api/pin/?theme=vue-dark&username=andre-fajar-n&repo=Goal_Keeper_Robot",
+        "image": "https://github-readme-stats.vercel.app/api/pin/?hide_border=true&theme=vue-dark&username=andre-fajar-n&repo=Goal_Keeper_Robot",
         "demo": "https://drive.google.com/file/d/1142XK3O3LKlO1i8NIfdUUYKsndvH3FjL/view?usp=sharing",
         "category": category[1],
         "github": "https://github.com/andre-fajar-n/Goal_Keeper_Robot"
