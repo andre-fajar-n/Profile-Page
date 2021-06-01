4ec856a1f144c42b936c679d8b6a3a7f0e4dceda
Andre Fajar N <62334317+andre-fajar-n@users.noreply.github.com>
2021-06-01 20:51:06 +0700

    add new tab in social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,12 +11,12 @@ export const Footer = () => {
         <div className="row mb-5">
           <div className="col-md-12 text-center social-media">
             <p>
-              <a href="https://www.facebook.com/andrefajarn/" className="social-item"><i className="fab fa-facebook"></i></a>
-              <a href="https://www.instagram.com/andrefajarn/" className="social-item"><i className="fab fa-instagram"></i></a>
-              <a href="https://www.linkedin.com/in/andre-fajar-n/" className="social-item"><i className="fab fa-linkedin-in"></i></a>
-              <a href="https://github.com/andre-fajar-n/" className="social-item"><i className="fab fa-github"></i></a>
-              <a href="mailto:andrenugroho395@gmail.com" className="social-item"><i className="far fa-envelope"></i></a>
-              <a href="https://www.hackerrank.com/afajar" className="social-item"><i className="fab fa-hackerrank"></i></a>
+              <a href="https://www.facebook.com/andrefajarn/" target="blank" className="social-item"><i className="fab fa-facebook"></i></a>
+              <a href="https://www.instagram.com/andrefajarn/" target="blank" className="social-item"><i className="fab fa-instagram"></i></a>
+              <a href="https://www.linkedin.com/in/andre-fajar-n/" target="blank" className="social-item"><i className="fab fa-linkedin-in"></i></a>
+              <a href="https://github.com/andre-fajar-n/" target="blank" className="social-item"><i className="fab fa-github"></i></a>
+              <a href="mailto:andrenugroho395@gmail.com" target="blank" className="social-item"><i className="far fa-envelope"></i></a>
+              <a href="https://www.hackerrank.com/afajar" target="blank" className="social-item"><i className="fab fa-hackerrank"></i></a>
             </p>
           </div>
           <p className="col-12 text-center">
